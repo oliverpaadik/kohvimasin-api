919cf5e7c26956353238f5dd0f5efab77be36b88
oliverpaadik <oliverpaadik@hotmail.com>
2021-04-05 21:24:52 +0300

    update

--- app.py
+++ app.py
@@ -1,51 +1,68 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
 import os
 import datetime
+import pprint
+import pytz
 
 app = Flask(__name__)
+cors = CORS(app)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JSON_SORT_KEYS'] = False
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+password = os.environ['API_KEY']
 db = SQLAlchemy(app)
 
 from models import *
 
-<<<<<<< HEAD
-=======
-@app.route("/")
-def hello():
-    return "Hello World!"
-
->>>>>>> 55718b2... initial commit of v2
-@app.route("/add", methods=['POST'])
+@app.route("/data", methods=['POST'])
 def add_log():
-    event_name = request.json.get('event_name')
-    timestamp = request.json.get('timestamp')
-    machine = request.json.get('machine')
-    received = datetime.datetime.now().isoformat()
-    try:
-        log=Log(
-            event_name = event_name,
-            timestamp = timestamp,
-            machine = machine,
-            received = received
-        )
-        db.session.add(log)
-        db.session.commit()
-        return "Event added to log: {}".format(log.id)
-    except Exception as e:
-        return (str(e))
+    # print(dict(request.headers))
+    # print(request.data)
+    
+    if (request.authorization is None):
+        return Response(status=401)
 
-@app.route("/getall")
+    if (request.authorization.password == password):
+        timezone = pytz.timezone("Europe/Tallinn")
+        request.data = request.data.decode("utf-8")
+        # print(request.data)
+        ids = []
+        for json in request.json:
+            motor_name = json.get('motor_name')
+            timestamp = json.get('Start_time')
+            transaction_id = json.get('Tran_id')
+            machine = json.get('machine')
+            received = datetime.datetime.now(timezone).strftime("%Y-%m-%d %H:%M:%S")
+            try:
+                log = Log(
+                    motor_name=motor_name,
+                    timestamp=timestamp,
+                    transaction_id=transaction_id,
+                    machine=machine,
+                    received=received)
+                db.session.add(log)
+                db.session.commit()
+                ids.append(log.id)
+            except Exception as e:
+                return (str(e))
+        return "Event(s) with ID(s) added to log: {}".format(ids)
+    else:
+        return Response(status=401)
+
+
+@app.route("/data")
+@cross_origin()
 def get_all():
     try:
-        log=Log.query.all()
-        return  jsonify([e.serialize() for e in log])
+        log = Log.query.all()
+        return jsonify([e.serialize() for e in log])
     except Exception as e:
-	    return(str(e))
-
+        return(str(e))
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -1,28 +1,31 @@
 from app import db
 
+
 class Log(db.Model):
-	__tablename__ = 'log'
-	
-	id = db.Column(db.Integer, primary_key=True)
-	event_name = db.Column(db.String())
-	timestamp = db.Column(db.String())
-	machine = db.Column(db.String())
-	received = db.Column(db.String())
-	
-	def __init_(self, event_name, timestamp, machine, received):
-		self.event_name = event_name
-		self.timestamp = timestamp
-		self.machine = machine
-		self.received = received
-		
-	def __repr__(self):
-		return '<id {}>'.format(self.id)
-		
-	def serialize(self):
-		return {
-			'id': self.id, 
-			'event_name': self.event_name,
-			'timestamp': self.timestamp,
-			'machine': self.machine,
-			'received': self.received
-		}
+    __tablename__ = 'log'
+    id = db.Column(db.Integer, primary_key=True)
+    motor_name = db.Column(db.String())
+    timestamp = db.Column(db.String())
+    transaction_id = db.Column(db.Integer())
+    machine = db.Column(db.String())
+    received = db.Column(db.String())
+
+    def __init_(self, motor_name, timestamp, transaction_id, machine, received):
+        self.motor_name = motor_name
+        self.timestamp = timestamp
+        self.transaction_id = transaction_id
+        self.machine = machine
+        self.received = received
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'motor_name': self.motor_name,
+            'timestamp': self.timestamp,
+            'transaction_id': self.transaction_id,
+            'machine': self.machine,
+            'received': self.received
+        }
